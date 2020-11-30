36b68fcd8ddccbcb79dab9c3399d4ca9be245a0e
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:47:03 +0300

    feat: add key prop

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -5,8 +5,8 @@ const Slides = props => {
   const { slides } = props;
   return (
     <div className="slides-container">
-      {slides.map(slide => (
-        <Slide slide={slide} />
+      {slides.map((slide, index) => (
+        <Slide key={`slide ${index}`} slide={slide} />
       ))}
     </div>
   );
