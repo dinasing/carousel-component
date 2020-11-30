09c030536cbaa9f72562e552743bcb57b3e46fed
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:43:25 +0300

    feat: add carousel component to app

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,12 +3,18 @@ import LeftControl from './LeftControl';
 import RightControl from './RightControl';
 import Slides from './Slides';
 
+const slides = [
+  { description: '', heading: '', image: '../../public/images/chihiro032' },
+  { description: '', heading: '', image: '../../public/images/chihiro043' },
+  { description: '', heading: '', image: '../../public/images/chihiro046' },
+];
+
 export default class Carousel extends Component {
   render() {
     return (
       <>
         <div className="carousel">
-          <Slides slide={slides} />
+          <Slides slides={slides} />
         </div>
         <LeftControl />
         <RightControl />
