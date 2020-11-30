fba5a8ac81c16b3374f8b01f27c308c298b7ce25
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:19:20 +0300

    feat: left control component

---
+++ src/components/LeftControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftControl = props => {
+  return (
+    <div className="control right">
+      <div className="arrow right">&gt</div>
+    </div>
+  );
+};
+
+export default LeftControl;
