2ab468cc54301ad1958bddf5bf30b2c16a046543
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:19:37 +0300

    feat: add right control component

---
+++ src/components/RightControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightControl = props => {
+  return (
+    <div className="control right">
+      <div className="arrow right">&lt</div>
+    </div>
+  );
+};
+
+export default RightControl;
