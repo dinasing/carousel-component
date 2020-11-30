044ee945c68b9f379d5f3a093b5c2e74bce4e429
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:20:09 +0300

    feat: add controls to carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import LeftControl from './LeftControl';
+import RightControl from './RightControl';
 import Slides from './Slides';
 
 export default class Carousel extends Component {
   render() {
     return (
-      <div className="carousel">
-        <Slides slide={slides} />
-      </div>
+      <>
+        <div className="carousel">
+          <Slides slide={slides} />
+        </div>
+        <LeftControl />
+        <RightControl />
+      </>
     );
   }
 }
