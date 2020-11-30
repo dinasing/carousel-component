0b729d9c54f40f24b22e910dfb7e482c70f287f9
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 00:42:22 +0300

    fix: change slide prop to slides

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Carousel from './Carousel';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>My Carousel!</h1>
+        <Carousel />
       </div>
     );
   }
