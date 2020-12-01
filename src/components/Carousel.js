0602cc6f0beede2d6878fd191492155d4fc14b92
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-01 14:44:51 +0300

    feat: add templaate data

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import LeftControl from './LeftControl';
 import RightControl from './RightControl';
 import Slides from './Slides';
+import image1 from '../../assets/images/chihiro032.jpg';
+import image2 from '../../assets/images/chihiro043.jpg';
+import image3 from '../../assets/images/chihiro046.jpg';
 
 const slides = [
-  { description: '', heading: '', image: '../../public/images/chihiro032' },
-  { description: '', heading: '', image: '../../public/images/chihiro043' },
-  { description: '', heading: '', image: '../../public/images/chihiro046' },
+  {
+    description: '',
+    heading: '',
+    image: image1,
+  },
+  { description: '', heading: '', image: image2 },
+  { description: '', heading: '', image: image3 },
 ];
 
 export default class Carousel extends Component {
