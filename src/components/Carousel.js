9d493e29fa3027f121906200bd2de51bbaaafc7b
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-02 20:35:30 +0300

    feat: implement slide changing methods

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LeftControl from './LeftControl';
+import LinksContainer from './LinksContainer';
 import RightControl from './RightControl';
 import Slides from './Slides';
 
 export default class Carousel extends Component {
   state = {
     currentItemIndex: 0,
+    isEnabled: true,
   };
 
   changeCurrentItemIndexTo = index => {
@@ -13,6 +15,52 @@ export default class Carousel extends Component {
     this.setState({
       currentItemIndex: (slides.length + index) % slides.length,
     });
+  };
+
+  hideItem = () => {
+    this.setState(() => ({
+      isEnabled: false,
+    }));
+  };
+
+  showItem = () => {
+    this.setState({
+      isEnabled: true,
+    });
+  };
+
+  previousItem = () => {
+    const { currentItemIndex } = this.state;
+
+    this.hideItem();
+    this.changeCurrentItemIndexTo(currentItemIndex - 1);
+    this.showItem();
+  };
+
+  nextItem = () => {
+    const { currentItemIndex } = this.state;
+
+    this.hideItem();
+    this.changeCurrentItemIndexTo(currentItemIndex + 1);
+    this.showItem();
+  };
+
+  onLeftButtonClick = () => {
+    const { isEnabled } = this.state;
+    if (isEnabled) {
+      this.previousItem();
+    }
+  };
+
+  onRightButtonClick = () => {
+    const { isEnabled } = this.state;
+    if (isEnabled) {
+      this.nextItem();
+    }
+  };
+
+  goToSlide = index => {
+    this.changeCurrentItemIndexTo(index);
   };
 
   render() {
@@ -24,6 +72,11 @@ export default class Carousel extends Component {
         <div className="carousel">
           <Slides slides={slides} currentItemIndex={currentItemIndex} />
         </div>
+        <LinksContainer
+          goToSlide={this.goToSlide}
+          length={slides.length}
+          currentItemIndex={currentItemIndex}
+        />
         <LeftControl onLeftButtonClick={this.onLeftButtonClick} />
         <RightControl onRightButtonClick={this.onRightButtonClick} />
       </>
