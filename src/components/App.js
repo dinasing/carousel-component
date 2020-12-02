09c65175a780c2ce02711b38a9ed54d19b4ca3f0
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-02 12:08:24 +0300

    refactor: move slides object to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Carousel from './Carousel';
+import image1 from '../../assets/images/chihiro032.jpg';
+import image2 from '../../assets/images/chihiro043.jpg';
+import image3 from '../../assets/images/chihiro046.jpg';
+
+const slides = [
+  {
+    description: '',
+    heading: '',
+    image: image1,
+  },
+  { description: '', heading: '', image: image2 },
+  { description: '', heading: '', image: image3 },
+];
 
 class App extends Component {
   render() {
     return (
       <>
-        <Carousel />
+        <Carousel slides={slides} />
       </>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,19 +2,6 @@ import React, { Component } from 'react';
 import LeftControl from './LeftControl';
 import RightControl from './RightControl';
 import Slides from './Slides';
-import image1 from '../../assets/images/chihiro032.jpg';
-import image2 from '../../assets/images/chihiro043.jpg';
-import image3 from '../../assets/images/chihiro046.jpg';
-
-const slides = [
-  {
-    description: '',
-    heading: '',
-    image: image1,
-  },
-  { description: '', heading: '', image: image2 },
-  { description: '', heading: '', image: image3 },
-];
 
 export default class Carousel extends Component {
   state = {
@@ -23,6 +10,7 @@ export default class Carousel extends Component {
 
   render() {
     const { currentItemIndex } = this.state;
+    const { slides } = this.props;
 
     return (
       <>
