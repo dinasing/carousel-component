5c72349de240d09b5b421f7db6637620dfa3fe38
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-09 02:05:39 +0300

    feat: change links for dynamic number of slides

--- src/components/LinksContainer.js
+++ src/components/LinksContainer.js
@@ -2,16 +2,16 @@ import React from 'react';
 import LinkToSlide from './LinkToSlide';
 
 export default function LinksContainer(props) {
-  const { currentItemIndex, length } = props;
+  const { currentItemIndex, length, numberOfSlidesOnPage } = props;
 
   const links = [];
   for (let index = 1; index <= length; index++) {
     links.push(
       <LinkToSlide
         key={`link to slide # ${index}`}
-        isActive={currentItemIndex === index}
+        isActive={currentItemIndex === index + numberOfSlidesOnPage - 1}
         value={index}
-        goToSlide={() => props.goToSlide(index)}
+        goToSlide={() => props.goToSlide(index + numberOfSlidesOnPage - 1)}
       />
     );
   }
