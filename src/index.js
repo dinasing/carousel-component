98c81ccef052313463776400307dd8e62519de24
Dina Singatulina <dina.sing1212@gmail.com>
2020-12-09 21:08:13 +0300

    feat: register service worker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
